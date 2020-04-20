5067dbdb6f1a01b715356fbbba231a678a52a862
曾宇 <zengyu@honasoft.com>
2020-04-20 16:38:48 +0800

    修改测试

--- test/timeSpan.ts
+++ test/timeSpan.ts
@@ -4,4 +4,4 @@ const beginTime: dateTime = new dateTime("2020-4-1 11:22:55");
 const endTime: dateTime = new dateTime("2020-7-22 12:52:32");
 
 const span = new timeSpan(endTime, beginTime);
-console.log(span.totalSeconds);
+console.log(span);
